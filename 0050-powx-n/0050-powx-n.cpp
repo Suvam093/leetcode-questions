e0ab6f2f77c4e1c73de90e5468c702c5361cb774
Suvam093 <139885056+Suvam093@users.noreply.github.com>
2024-05-12 20:14:50 +0530

    Time: 2 ms (40.29%), Space: 7.9 MB (93.20%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1;
+        int a;
+        a= abs(n);
+        cout<<a;
+        while(a>0){
+            if(a&1) ans*=x;
+            x = x*x;
+            a>>=1;
+            
+        }
+        if(n<0) ans=1/ans;
+        return ans;
+    }
+};
