132f251ed060781677519df6eee44dfd6eb78feb
Suvam093 <139885056+Suvam093@users.noreply.github.com>
2024-05-20 22:16:32 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (28.03%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        
+ 
+           if(word[0]<'a'){                                                 
+              int cap = 0;
+              for(int i =0;i<word.size();i++)
+                  if(word[i]<'a')cap++;
+              if(cap>1 && cap< word.size())return false;                    
+           }
+           else
+               for(int i =0;i<word.size();i++)
+                   if(word[i]<'a')return false; 
+               
+           return true;
+    }  
+    
+};
